46bea3d77e8498b966bc2906acd95db4811b2ef8
dinesh9299 <dineshv9299@gmail.com>
2025-01-25 16:46:34 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use("/api", router);
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
     console.log("Connected to database");
   });
